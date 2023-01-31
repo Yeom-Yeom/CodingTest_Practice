fa863c8cd45a5c2036b3ded4e42bd74eda089a23
yeom-yeom <diting9813@skuniv.ac.kr>
2023-01-31 16:06:45 +0900

    23.01.31

--- Lv.0/page1/check_sqrt.py
+++ Lv.0/page1/check_sqrt.py
@@ -1,5 +1,5 @@
 import math # sqrt 함수를 사용하기 위한 math import
-def check_pow(n):
+def check_sqrt(n):
     if math.sqrt(n) == int(math.sqrt(n)): # n의 제곱근이 n의 제곱근의 integer 형과 동일하면 그 수는 제곱수.
         return 1
     else :
